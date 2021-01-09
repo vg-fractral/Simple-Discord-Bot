57d7b0811ba2192e7640ca0b76f9a8ad5538d8ec
Vilian <vi.gecoding@gmail.com>
2021-01-09 13:56:21 +0200

    added Misc

---
+++ Cogs/misc.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from random import choice
+from discord import File
+
+class Misc(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(help="Are you going to dare?",name="Hentai", aliases=["h, hen"])
+    async def hentai(self, ctx):
+        await ctx.channel.purge(limit=1)
+        await ctx.channel.send(file=File(choice(["./why.png", "nani.jpg"])))
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Misc cog loaded")
+
+
+def setup(client):
+    client.add_cog(Misc(client))
